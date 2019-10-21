9d699315205abc94785bf82741c0ffb114071cdf
Sarah Ohlin <sarahohlin1@gmail.com>
2019-10-21 14:48:08 -0400

    setup main bot logic

--- src/Main.hs
+++ src/Main.hs
@@ -2,7 +2,23 @@ module Main where
     
 import Discord
 import Discord.Types
+import System.IO (readFile)
+import Data.Text (unpack, pack, Text)
 
+import Dispatch
+
+-- | Reads the token from the .token file.
+getToken :: IO String
+getToken = (return . head . lines) =<< readFile ".token"
+
+-- | Reads in a token,, and runs the discord bot.
+bot :: String -> IO ()
+bot token = do
+    res <- runDiscord $ def { discordToken = pack token
+                            , discordOnEvent = dispatch }
+    putStrLn $ unpack res
+    return ()
+    
+-- | Starts the bot-- entry point.
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = putStrLn "Starting..." >> getToken >>= bot
