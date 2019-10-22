e2f5ce736367af902e541ab2ca120a7d86b07be6
Sarah Ohlin <sarahohlin1@gmail.com>
2019-10-21 20:34:18 -0400

    added command dispatch module

---
+++ src/Commands.hs
@@ -0,0 +1,35 @@
+-- | All defined commands the bot accepts.
+module Commands
+( runCommand
+) where
+    
+import Discord
+import Discord.Types
+import qualified Discord.Requests as R
+
+import Data.Map
+import Prelude hiding (lookup)
+
+-- | The type of a command handler
+type CommandHandle = DiscordHandle -> Message -> [String] -> IO (Maybe String)
+    
+-- | The main map of commands to their functions
+commands :: Map String CommandHandle
+commands = fromList $ [ ("help", help)
+                      , ("openBook", openBook)
+                      ]
+
+-- | Main export, runs a command given with its args
+runCommand :: DiscordHandle -> Message -> [String] -> IO (Maybe String)
+runCommand _ _ [] = error $ "No args given to runCommand!"
+runCommand dis msg args =
+    case lookup (head args) commands of
+        Nothing -> return $ Just "Command not found."
+        Just fn -> fn dis msg (tail args)
+    
+-- * COMMANDS START HERE
+help :: CommandHandle
+help dis msg args = pure Nothing
+
+openBook :: CommandHandle
+openBook dis msg args = pure Nothing
